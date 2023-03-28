7441005be77257dac1a3ef58c10e706c73ab3fdb
pong game <a3938081@gamil.com>
2023-03-28 20:14:27 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const buttons = document.querySelectorAll("button");
+let num1 = "";
+let num1Before = "";
+let num2 = "";
+let num2Before = "";
+let bool = true;
+let symbol = "";
+for (const b of buttons) {
+    b.addEventListener("click", function () {
+        let value = b.textContent;
+        if (value === '*' || value === '-' || value === '+' || value === '/') {
+            symbol = value;
+            document.getElementById("input").value = "";
+            bool = false;
+        } else if (value === "c") {
+            clear();
+            document.getElementById("input").value = "";
+        } else if (value === '=') {
+            document.getElementById("input").value = calc(num1, num2, symbol);
+            clear();
+        } else if (bool) {
+            if (value === 'b') {
+                num1 = num1Before;
+                document.getElementById("input").value = num1Before;
+            } else {
+                document.getElementById("input").value = num1 + value;
+                num1Before = num1;
+                num1 += b.textContent;
+            }
+
+        } else {
+            if (value === 'b') {
+                alert(num2Before);
+                document.getElementById("input").value = num2Before;
+                num2 = num2Before;
+            } else {
+                document.getElementById("input").value = num2 + value;
+                num2Before = num2;
+                num2 += b.textContent;
+            }
+        }
+
+    })
+
+}
+
+function calc(num1, num2, symbol) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    if (symbol === "+") {
+        return num1 + num2;
+    } else if (symbol === "*") {
+        return num1 * num2;
+    }
+    if (symbol === "-") {
+        return num1 - num2;
+    } else if (symbol === "/") {
+        return num1 / num2;
+    }
+}
+
+function clear() {
+    num1 = "";
+    num2 = "";
+    bool = true;
+}
